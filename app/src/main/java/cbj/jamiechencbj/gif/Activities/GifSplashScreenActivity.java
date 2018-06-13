3201f2cdaaa627a225e0446f28d38786baf7165d
Baijian <baijian8906@gmail.com>
2018-06-13 16:37:24 -0400

    - Updated Splash screen

---
+++ app/src/main/java/cbj/jamiechencbj/gif/Activities/GifSplashScreenActivity.java
@@ -0,0 +1,18 @@
+package cbj.jamiechencbj.gif.Activities;
+
+import android.os.Bundle;
+
+import cbj.jamiechencbj.gif.Core.GifBaseActivity;
+import cbj.jamiechencbj.gif.R;
+
+public class GifSplashScreenActivity extends GifBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gif_splash_screen);
+    }
+
+
+
+}
